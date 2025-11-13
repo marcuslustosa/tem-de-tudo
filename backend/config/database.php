da278553b7308c0f30e4313fa955b0fb4c109666
marcuslustosa <marcuslustosa@gmail.com>
2025-11-13 11:55:28 -0300

    fix: correcao host PostgreSQL e URLs para Render

--- backend/config/database.php
+++ backend/config/database.php
@@ -98,6 +98,9 @@ return [
             'sslmode' => env('DB_SSLMODE', 'require'),
             'options' => [
                 PDO::ATTR_EMULATE_PREPARES => true,
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_PERSISTENT => false,
             ],
         ],
 
